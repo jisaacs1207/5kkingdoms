774b50aefc67102907346285ba0d6ce533c2bb1c
Joshua Isaacs <jisaacs1207@gmail.com>
2014-08-18 22:23:26 +0300

    Cleanup and a fix for light hearts and debugging messages, as well as a tech speed change. Pushed version 1.0

--- src/main/java/io/github/jisaacs1207/kingdoms5k/Kingdoms5k.java
+++ src/main/java/io/github/jisaacs1207/kingdoms5k/Kingdoms5k.java
@@ -1,39 +1,16 @@
 package io.github.jisaacs1207.kingdoms5k;
 
-import java.util.Iterator;
-
-
-
-
-
-
-
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
-
-
-
-
-
-
-
-
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
@@ -46,36 +23,6 @@ public final class Kingdoms5k extends JavaPlugin implements Listener{
 		saveDefaultConfig();
 		getWorldGuard();
 		getServer().getPluginManager().registerEvents(this, this);
-		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-            	for(Player player : Bukkit.getOnlinePlayers()){
-            		Location loc = player.getLocation(); 
-            		RegionManager regionManager = getWorldGuard().getRegionManager(loc.getWorld());
-            		ApplicableRegionSet set = regionManager.getApplicableRegions(loc);
-            		for (ProtectedRegion region : set){
-            			String name = region.getId();
-            			if(name.equalsIgnoreCase(getConfig().getString("name1"))){
-            				// perk1
-            			}
-            			else if(name.equalsIgnoreCase(getConfig().getString("name2"))){
-            				// perk2
-            			}
-            			else if(name.equalsIgnoreCase(getConfig().getString("name3"))){
-            				// perk3
-            			}
-            			else if(name.equalsIgnoreCase(getConfig().getString("name4"))){
-            				// perk4
-            				
-            			}
-            			else if(name.equalsIgnoreCase(getConfig().getString("name5"))){
-            				// perk5
-            			}
-            		}
-            	}
-            }
-        }, 0L, 20L);
 	}
 	
 	@EventHandler(priority=EventPriority.HIGHEST)
@@ -107,22 +54,24 @@ public final class Kingdoms5k extends JavaPlugin implements Listener{
 			}
 		}
 		// Tech
-		if(techSpeed==true)player.setWalkSpeed(0.4F);
+		if(player.getWalkSpeed()<0.4F){
+			if(techSpeed==true)player.setWalkSpeed(0.4F);
+		}
+		
 		if(techSpeed==false)player.setWalkSpeed(0.2F);
 		
 		// Light
 		if(lightLight==true){
 			player.setMaxHealth(30);
-			if (Math.random() * 100 >= 99.3){
-				player.sendMessage("light");
+			if (Math.random() * 100 >= 99.2){
 				player.setHealth(player.getHealth()+1);
 				
 			}
-			if (Math.random() * 100 >= 99.5){
-				player.setExhaustion(player.getExhaustion()+1);
+			if (Math.random() * 100 >= 99.2){
+				player.setFoodLevel(player.getFoodLevel()+1);
 			}
 		}
-		if(lightLight==false)player.resetMaxHealth();
+		if(lightLight==false)player.setMaxHealth(20);
 		
 		// Dark
 		if(darkDark==true){
@@ -171,23 +120,6 @@ public final class Kingdoms5k extends JavaPlugin implements Listener{
 			}
 		}
 	}
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void playerCombust( EntityCombustEvent event) {
-		if(event.getEntity() instanceof Player){
-			Player player = (Player) event.getEntity();
-			Location loc = player.getLocation(); 
-			RegionManager regionManager = getWorldGuard().getRegionManager(loc.getWorld());
-			ApplicableRegionSet set = regionManager.getApplicableRegions(loc);
-			for (ProtectedRegion region : set){
-				String name = region.getId();
-				player.sendMessage(name);
-				if(name.equalsIgnoreCase(getConfig().getString("name5"))){
-					event.setCancelled(true);
-				}
-			}
-		}
-	}
-	
 	
 	private WorldGuardPlugin getWorldGuard() {
 	    Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
